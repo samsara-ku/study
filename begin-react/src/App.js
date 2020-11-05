c4fa0fb0ca42c44c33d0b69643531f0724981b53
yunhoe <yunhoe@deepnatural.ai>
2020-11-06 00:47:35 +0900

    useState with input tag

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -4,9 +4,10 @@ import logo from './logo.svg'
 import './App.css'
 import Wrapper from './Wrapper'
 import Counter from './Counter'
+import InputSample from './InputSample'
 
 function App() {
-  return <Counter />
+  return <InputSample />
 }
 
 export default App
