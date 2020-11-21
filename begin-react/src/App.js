1e29be2dd28c8ad80dd1a671bf7746c7fcdcc80b
yunhoe <yunhoe@deepnatural.ai>
2020-11-21 12:38:03 +0900

    Added list using setUser is completed

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,13 +1,68 @@
-import React from 'react'
-import Hello from './Hello'
-import logo from './logo.svg'
-import './App.css'
-import Wrapper from './Wrapper'
-import Counter from './Counter'
-import InputSample from './InputSample'
+import React, { useRef } from 'react'
+import UserList from './UserList'
+import CreateUser from './CreateUser'
+import { useState } from 'react'
 
 function App() {
-  return <InputSample />
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  })
+
+  const { username, email } = inputs
+
+  const onChange = (e) => {
+    const { name, value } = e.target
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    })
+  }
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+    },
+  ])
+
+  const nextId = useRef(4)
+
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    }
+
+    setUsers([...users, user])
+    //setUsers(users.concat(user))
+
+    setInputs({
+      username: '',
+      email: '',
+    })
+
+    nextId.current += 1
+  }
+  return (
+    <>
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+      <UserList users={users} />
+    </>
+  )
 }
 
 export default App
